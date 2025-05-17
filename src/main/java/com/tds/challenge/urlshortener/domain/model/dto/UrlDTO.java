7cda69e5505e1339c92021880661235274297ee3
Samir  Andrade <samirandradee@gmail.com>
2025-05-17 00:22:10 -0300

    feat: criação do DTO UrlDTO e UrlShortenRequestDTO

---
+++ src/main/java/com/tds/challenge/urlshortener/domain/model/dto/UrlDTO.java
@@ -0,0 +1,19 @@
+package com.tds.challenge.urlshortener.domain.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UrlDTO {
+    private String originalUrl;
+    private String shortUrl;
+    private long accessCount;
+    private LocalDateTime createdAt;
+}
