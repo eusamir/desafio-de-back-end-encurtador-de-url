d745278a098c80bf6f024c0daa2da18986000596
Samir  Andrade <samirandradee@gmail.com>
2025-05-17 13:16:00 -0300

    fix: adicionando @Valid na interface IUrlController para evitar conflito com implementação

--- src/main/java/com/tds/challenge/urlshortener/controller/IUrlController.java
+++ src/main/java/com/tds/challenge/urlshortener/controller/IUrlController.java
@@ -10,6 +10,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -85,7 +86,7 @@ public interface IUrlController {
             )
     )
     @PostMapping("/shorten")
-    ResponseEntity<UrlDTO> shortenUrl(@RequestBody UrlShortenRequestDTO request, HttpServletRequest servletRequest);
+    ResponseEntity<UrlDTO> shortenUrl(@Valid @RequestBody UrlShortenRequestDTO request, HttpServletRequest servletRequest);
 
     @Operation(
             summary = "Redirecionar URL encurtada",
